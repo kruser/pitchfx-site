b3c4bd9c7bf1b0da2430ec47fc947215a46a461a
kruser <ryan@kruseonline.net>
2013-09-10 08:52:47 -0500

    use compression for all web service responses and add a static content cache.

--- app/app.js
+++ app/app.js
@@ -8,12 +8,16 @@ var atbats = require('./routes/atbats');
 var players = require('./routes/players');
 
 var app = express();
+app.use(express.compress());
+app.use(express.staticCache());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/api/atbats', atbats.query);
 app.get('/api/players', players.query);
 app.get('/api/players/:id', players.getPlayer);
 
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
