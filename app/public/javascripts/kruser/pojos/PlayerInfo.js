c23c8e9cca6b93983d66b999888ce7eb4f9e17b8
kruser <ryan@kruseonline.net>
2013-10-11 20:59:02 -0500

    date formatting for birthdate and mlb debut date

--- app/public/javascripts/kruser/pojos/PlayerInfo.js
+++ app/public/javascripts/kruser/pojos/PlayerInfo.js
@@ -14,8 +14,20 @@ kruser.pojos.PlayerInfo = function(dataObj) {
 /**
  * @memberof kruser.pojos.PlayerInfo
  * @instance
- * @returns {string} the full name of the player
+ * @returns {Date} the birthday
  */
-kruser.pojos.Player.prototype.getFullName = function() {
-    return this.first + ' ' + this.last;
+kruser.pojos.PlayerInfo.prototype.getBirthDate = function() {
+    return moment(this.birth_date).toDate();
+}
+
+/**
+ * @memberof kruser.pojos.PlayerInfo
+ * @instance
+ * @returns {Date} the mlb debut date
+ */
+kruser.pojos.PlayerInfo.prototype.getMlbDebut = function() {
+    if (this.pro_debut_date) {
+        return moment(this.pro_debut_date).toDate();
+    }
+    return undefined;
 }
