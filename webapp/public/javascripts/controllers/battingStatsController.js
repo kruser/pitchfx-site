9a8270b471cd0445b672af538a7504f28eabd976
kruser <ryan@kruseonline.net>
2013-11-29 17:29:36 -0600

    base filters

--- webapp/public/javascripts/controllers/battingStatsController.js
+++ webapp/public/javascripts/controllers/battingStatsController.js
@@ -17,6 +17,13 @@ controllers.battingStatsController = [ '$scope', '$log', 'playerService', 'stats
             start_moment : moment('2010-01-01'),
             end : moment().format('YYYY-MM-DD'),
             end_moment : moment().add('days', 1),
+        },
+        runners : {
+            gate : 'OR',
+            empty : false,
+            first : false,
+            second : false,
+            third : false,
         }
     };
 
@@ -24,17 +31,17 @@ controllers.battingStatsController = [ '$scope', '$log', 'playerService', 'stats
      * Sets up all the watchers on filter variables
      */
     function setupWatchers() {
-        $scope.$watch('filters.pitcherHand', function(filters) {
+        $scope.$watch('[filters.pitcherHand, filters.runners.gate, filters.runners.empty, filters.runners.first, filters.runners.second, filters.runners.third]', function(filters) {
             $scope.runStats();
-        });
+        }, true);
         $scope.$watch('filters.date.start', function(filters) {
             $scope.filters.date.start_moment = moment($scope.filters.date.start);
             $scope.runStats();
-        });
+        }, true);
         $scope.$watch('filters.date.end', function(filters) {
             $scope.filters.date.end_moment = moment($scope.filters.date.end).add('days', 1);
             $scope.runStats();
-        });
+        }, true);
     }
 
     /**
@@ -101,6 +108,42 @@ controllers.battingStatsController = [ '$scope', '$log', 'playerService', 'stats
         if ($scope.filters.pitcherHand && atbat.p_throws !== $scope.filters.pitcherHand) {
             return false;
         } else if ($scope.filters.date.start_moment.isAfter(atbat.start_tfs_zulu) || $scope.filters.date.end_moment.isBefore(atbat.start_tfs_zulu)) {
+            return false;
+        } else if (!$scope.passesRunnerFilter(atbat)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * checks to see if an at-bat passes only the runners filter object
+     * 
+     * @param {*}
+     *            atbat - the atbat to check against the scope's filter object
+     * @returns {Boolean} true if the atbat passes the filters
+     */
+    $scope.passesRunnerFilter = function(atbat) {
+        var filter = $scope.filters.runners;
+        if (atbat.pitch && atbat.pitch.length > 0 && (filter.empty || filter.first || filter.second || filter.third)) {
+            var lastPitch = atbat.pitch[atbat.pitch.length - 1];
+            if (filter.gate === 'OR') {
+                return ((lastPitch.on_1b && filter.first) || (lastPitch.on_2b && filter.second) || (lastPitch.on_3b && filter.third) || (filter.empty && !lastPitch.on_1b && !lastPitch.on_2b && !lastPitch.on_3b));
+            }
+            if (filter.gate === 'AND') {
+                if (filter.first && !lastPitch.on_1b) {
+                    return false;
+                }
+                if (filter.second && !lastPitch.on_2b) {
+                    return false;
+                }
+                if (filter.third && !lastPitch.on_3b) {
+                    return false;
+                }
+                if (filter.empty && (lastPitch.on_1b || lastPitch.on_2b || lastPitch.on_3b)) {
+                    return false;
+                }
+                return true;
+            }
             return false;
         }
         return true;
