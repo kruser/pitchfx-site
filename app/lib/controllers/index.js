e60c27119162ed66028793672e546d8bbee863b3
kruser <ryan@kruseonline.net>
2014-01-27 23:30:53 -0600

    stub out the player view

--- app/lib/controllers/index.js
+++ app/lib/controllers/index.js
@@ -1,25 +1,31 @@
 'use strict';
-
 var path = require('path');
+var mlb = require('../services/mlb');
 
+/**
+ * Loads all partials from the partials folder
+ */
 exports.partials = function(req, res) {
-  var stripped = req.url.split('.')[0];
-  var requestedView = path.join('./', stripped);
-  res.render(requestedView, function(err, html) {
-    if(err) {
-      res.render('404');
-    } else {
-      res.send(html);
-    }
-  });
-};
-
-exports.index = function(req, res) {
-  res.render('index');
+    var stripped = req.url.split('.')[0];
+    var requestedView = path.join('./', stripped);
+    res.render(requestedView, function(err, html) {
+        if (err) {
+            res.render('404');
+        } else {
+            res.send(html);
+        }
+    });
 };
 
 /**
- * GET player page.
+ * Render the home page
+ */
+exports.index = function(req, res) {
+    res.render('index');
+};
+
+/**
+ * Renders the player pages
  */
 exports.player = function(req, res) {
     var playerId = req.params.playerId;
