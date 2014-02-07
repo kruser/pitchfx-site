2d539d7868128dbdfac4f67db6de0cf2a78738cc
kruser <ryan@kruseonline.net>
2014-02-07 13:51:24 -0600

    harden playercard types

--- app/app/scripts/controllers/filtersController.js
+++ app/app/scripts/controllers/filtersController.js
@@ -6,19 +6,19 @@ var controllers = controllers || {};
 controllers.filtersController = [ '$scope', '$log', '$timeout', '$angularCacheFactory', '$routeParams', '$route', '$location', 'filtersService', function($scope, $log, $timeout, $angularCacheFactory, $routeParams, $route, $location, filtersService) {
     var filterCache = $angularCacheFactory('filterCache', {
         storageMode : 'localStorage',
-        maxAge: 3600000,
-        deleteOnExpire: 'aggressive',
-        recycleFreq: 60000,
-        cacheFlushInterval: 3600000,
+        maxAge : 3600000,
+        deleteOnExpire : 'aggressive',
+        recycleFreq : 60000,
+        cacheFlushInterval : 3600000,
     });
-    
+
     $log.debug($routeParams);
     if ($routeParams.playerCard) {
         $scope.playerCard = $routeParams.playerCard;
-    }  else {
+    } else {
         $scope.playerCard = ($scope.playerPosition === '1') ? 'pitcher' : 'batter';
     }
-    
+
     var filtersFromCache = filterCache.get('filters');
     if (filtersFromCache && filtersFromCache.length > 0) {
         $scope.filters = filtersFromCache[0];
@@ -65,33 +65,36 @@ controllers.filtersController = [ '$scope', '$log', '$timeout', '$angularCacheFa
             }
         };
     }
-    
+
     $scope.$watch('playerCard', function(playerCard) {
-        if ($routeParams.playerCard !== playerCard){
-            $location.path('/' + $route.current.$$route.meta + '/' + playerCard);  
+        if ($routeParams.playerCard !== playerCard) {
+            $location.path('/' + $route.current.$$route.meta + '/' + playerCard);
         }
     }, true);
 
     $scope.$watch('[filters]', function(filters) {
-        filterCache.put('filters', filters);
-        filtersService.filters = filters;
+        if ($scope.playerCard) {
+            filterCache.put('filters', filters);
+            filtersService.filters = filters;
+        }
     }, true);
-    
+
     /**
      * Gets the recommended starting date for the filters.
      * 
-     * If we're in May or later, the starting date will be the 1st of the current year.
-     * If we're earlier than May, the starting date will be the 1st of the prior year.
+     * If we're in May or later, the starting date will be the 1st of the
+     * current year. If we're earlier than May, the starting date will be the
+     * 1st of the prior year.
      */
     function getStartingDate() {
         var currentMonth = moment().month();
         var currentYear = moment().year();
         if (currentMonth < 4) {
             /* last year */
-            return moment([currentYear - 1, 0, 1]).format('YYYY-MM-DD');
+            return moment([ currentYear - 1, 0, 1 ]).format('YYYY-MM-DD');
         } else {
             /* this year */
-            return moment([currentYear, 0, 1]).format('YYYY-MM-DD');
+            return moment([ currentYear, 0, 1 ]).format('YYYY-MM-DD');
         }
     }
 
