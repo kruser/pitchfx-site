7bb77681e89d4404bc01b6017075b69047ca4548
kruser <ryan@kruseonline.net>
2014-03-01 23:57:34 -0600

    player tests

---
+++ app/test/spec/pojos/Player.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('POJO: Player', function() {
+    it('Test player full name', function() {
+        var player = new pitchfx.Player({
+            "_id" : "52ea94c4ccd240a2510f982a",
+            "first" : "Josmil",
+            "last" : "Pinto",
+            "id" : 500887
+        });
+        expect(player.getFullName()).toBe('Josmil Pinto');
+    });
+
+    it('Test player url', function() {
+        var player = new pitchfx.Player({
+            "_id" : "52ea94c4ccd240a2510f982a",
+            "first" : "Josmil",
+            "last" : "Pinto",
+            "id" : 500887
+        });
+        expect(player.getUrlFriendlyName()).toBe('josmil-pinto');
+    });
+});
